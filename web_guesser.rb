34a2c8e1a1aa4e92e94199f878f71cbd155c7da8
A W <waq.mmhy@live.ca>
2018-11-17 22:47:03 -0500

    Add guess limiting functionality

--- web_guesser.rb
+++ web_guesser.rb
@@ -15,30 +15,43 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-NUMBER = rand(100)
+set :NUMBER, rand(100)
 msg = []
 bgcolour = 'white'
+@@triesleft = 5
 
 get '/' do
     guess = params['guess'].to_i
     msg = check(guess)
-    erb :index, :locals => {:number => NUMBER, :msg => msg[0], :bgcolour => msg[1]}
+    erb :index, :locals => {:number => settings.NUMBER, :msg => msg[0], :bgcolour => msg[1], :triesleft => @@triesleft}
 end 
 
 def check(x) 
-    case 
-    when x > NUMBER + 5
-        ['Way too high!', 'red']
-    when x > NUMBER
-        ['Too high!', 'pink']
-    when x < NUMBER - 5 
-        ['Way too low!', 'red']
-    when x < NUMBER 
-        ['Too low!', 'pink']
-    when x == NUMBER 
-        ['You got it right! The secret number is ' + NUMBER.to_s, 'green']
+    @@triesleft -= 1
+
+    if @@triesleft == 0
+        settings.NUMBER = rand(100)
+        @@triesleft = 5
+        return ['Sorry, you lost. Try again with a new number!', 'white']
     else 
-        'Not a valid input!'
+        case 
+        when x == 0
+            ['', '']
+        when x > settings.NUMBER + 5
+            ['Way too high!', 'red']
+        when x > settings.NUMBER
+            ['Too high!', 'pink']
+        when x < settings.NUMBER - 5 
+            ['Way too low!', 'red']
+        when x < settings.NUMBER 
+            ['Too low!', 'pink']
+        when x == settings.NUMBER 
+            ['You got it right! The secret number is ' + settings.NUMBER.to_s, 'green']
+        else 
+            'Not a valid input!'
+        end
     end
+
+   
 end
 
